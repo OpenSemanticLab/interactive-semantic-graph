7c70d87b639fd858df903978bb78f4ec04efe2e2
Lukas Scheuplein <lukas.scheuplein@isc.fraunhofer.de>
2023-09-08 06:57:29 +0000

    Changed styling

--- src/GraphTool/GraphToolLegend.js
+++ src/GraphTool/GraphToolLegend.js
@@ -1,111 +1,111 @@
 
     //repeats the invisibility of properties that are set invisible in the legend
     function repeatInvisibility(options) {
-  
-  
+
+
         for (const [key, value] of Object.entries(options.groups)) {
-    
+
           for (const [subKey, subValue] of Object.entries(value)) {
             if (subValue == true) {
-    
+
               let objectToRepeat = {
                 repeat: key
               }
-    
+
               this.legendFunctionality(objectToRepeat);
-    
+
             }
           }
         }
-    
-    
+
+
         // Array.from(options.groups).forEach((group)=> {
-    
+
         // });
-    
+
         // let legend = document.getElementById("legendContainer");
         // let children = Array.from(legend.children);
-    
+
         // children.forEach(child => {
-    
+
         //   if(window.getComputedStyle(child.children[1]).backgroundColor == "rgb(255, 255, 255)"){
-    
+
         //     let objectToRepeat = {repeat:child.children[1].innerHTML}
-    
+
         //     this.legendFunctionality(objectToRepeat);
-    
+
         //   }
-    
+
         // });
-    
+
       }
 
     //sets the color of the legend white if the group is set to hidden = true
     function setInvisibleLegendGroupsWhite(invisibleGroups) {
-  
+
         let legend = document.getElementById(this.prefix + "legendContainer");
         let children = Array.from(legend.children);
-    
-    
+
+
         children.forEach((child) => {
-    
+
           if (invisibleGroups.includes(child.children[1].innerHTML)) {
-    
+
             child.children[1].style.background = "rgb(255, 255, 255)"
-    
+
           }
-    
+
         });
-    
+
       }
 
     // resets nodes and edges visibility
     function resetNodesAndEdgesVisibility() {
-  
+
         this.nodes.forEach((node) => {
           node.hidden = false;
           node.physics = !node.hidden;
           node.visited = false;
           this.nodes.update(node);
         });
-    
+
         this.edges.forEach((edge) => {
           edge.hidden = false;
           edge.physics = !edge.hidden;
           this.edges.update(edge);
         });
-    
+
       }
 
     //generates the legend for the graph
     function createLegend() {
         var invisibleGroups = [];
-    
-    
+
+
         //if (!document.getElementById("legendContainer")) {
-    
+
           Object.keys(this.drawer.colorObj).forEach((key) => {
-    
-    
+
+
             if(!this.options.groups[key]){
-    
+
               this.options.groups[key] = {
                 hidden: false
               };
-    
+
             }
             // options.groups[key] = {
             //   hidden: false
             // };
-    
+
           });
-    
+
         //}
-    
+
         if (document.getElementById(this.prefix + "legendContainer")) {
-    
+
           invisibleGroups = this.legendInvisibleGroups(this.options);
-    
+
           document.getElementById(this.prefix + "legendContainer").remove();
         }
         var legendDiv = document.createElement("div");
@@ -114,14 +114,15 @@
         legendDiv.style.width = '100%';
         legendDiv.style.position = 'relative';
         legendDiv.style.display = 'inline-block';
+        legendDiv.style = 'padding: 8px 0;';
         legendDiv.id = this.prefix + "legendContainer";
         var legendColors = this.drawer.colorObj
         var legendSet = {}
-    
+
         for (let edge of this.edges.get()) {
-    
+
           if (!legendSet[edge.group]) {
-    
+
             //legendColors[input.properties[i]] = colors[i];
             var propertyContainer = document.createElement("div");
             var propertyColor = document.createElement("div");
@@ -130,45 +131,44 @@
             propertyContainer.id = this.prefix + edge.label;
             propertyColor.className = "color-container";
             propertyName.className = "name-container";
-            propertyColor.style.float = "left";
+            propertyColor.style = "float: left; border-radius: 50px; width: 20px; height: 20px; margin-right: 4px; margin-left: 16px;";
             propertyName.style.float = "left";
-            propertyColor.style.border = "1px solid black";
-            propertyName.style.border = "1px solid black";
+            propertyName.style.marginRight = "24px;";
+            propertyName.style.fontSize = "0.8rem;";
+            // propertyColor.style.border = "1px solid black";
+            // propertyName.style.border = "1px solid black";
             propertyColor.style.background = legendColors[edge.group]
             propertyColor.innerHTML = "";
             propertyName.innerHTML = edge.label;
-            propertyColor.style.width = "30px";
-            propertyColor.style.height = "30px";
-            propertyName.style.height = "30px";
-            propertyName.style.background = '#DEF';
-            //propertyName.text-align = 'center';
-            propertyContainer.paddinng = '5px 5px 5px 5px';
+            // propertyName.style.background = '#DEF';
+            // propertyName.text-align = 'center';
+            // propertyContainer.padding = '5px 5px 5px 5px';
             propertyName.addEventListener("click", (e) => this.legendFunctionality(e));
             propertyColor.addEventListener("click", (e) => this.legendFunctionality(e));
             legendDiv.append(propertyContainer);
             propertyContainer.append(propertyColor);
             propertyContainer.append(propertyName);
-    
+
             legendSet[edge.group] = legendColors[edge.group];
           }
         }
-    
-    
+
+
         this.setInvisibleLegendGroupsWhite(invisibleGroups);
       }
 
     //function to set nodes and edges hidden when legend is clicked
     function setNodeVisibilityByVisiblePath(nodeId, rootNodeId){
-  
+
         for(let i = 0; i < this.configFile.root_node_objects.length; i++) {
           if(nodeId == 'jsondata/' + this.configFile.root_node_objects[i].node_id) {
-            
+
             return true;
-    
+
           }
         }
         //if (nodeId == this.drawer.rootId) return true; //root is always visible
-    
+
         let node = this.nodes.get(nodeId);
         if (node.visited) return !node.hidden //prevent circles. ToDo: Reuse results between runs
         node.visited = true;
@@ -192,17 +192,17 @@
 
       //turns clicked properties of the legend invisible or back to visible
     function legendFunctionality(e){
-  
-  
+
+
         let legendGroup;
         let group;
         let nodeChildren;
         let strategy = "strategy2"
-    
-    
+
+
         //this.updatePositions()
         if (strategy == "strategy2") {
-    
+
           if (!e.repeat) {
             legendGroup = e.target.parentNode.childNodes[1].innerHTML;
             //A node is visible if at least one path over visible edges to the root node exists.
@@ -216,14 +216,14 @@
             edge.physics = !edge.hidden;
             this.edges.update(edge); //see also: https://visjs.github.io/vis-network/examples/network/data/datasets.html
           });
-    
+
           //reset nodes
           this.nodes.forEach((node) => {
             node.hidden = false;
             node.physics = !node.hidden;
             node.visited = false;
           });
-    
+
           //check each node
           this.nodes.forEach((node) => {
             this.setNodeVisibilityByVisiblePath(node.id, 0)
@@ -231,23 +231,23 @@
             this.nodes.forEach((node) => {
               node.visited = false;
             });
-    
+
             this.nodes.update(node); //see also: https://visjs.github.io/vis-network/examples/network/data/datasets.html
           });
         }
-    
+
         var allFalse = Object.keys(this.options.groups).every((k) => {
           if (k === 'useDefaultGroups') {
             return true
           }
           return this.options.groups[k].hidden === false
         });
-    
+
         if (allFalse === true) {
           /*oldGroups = {};*/
         }
-    
-    
+
+
       };
 
 

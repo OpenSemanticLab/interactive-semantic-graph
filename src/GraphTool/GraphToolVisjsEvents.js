59aefba41e87103c5bf392d62f1da77751874dd9
AlexanderPraktikum2021 <AlexanderPraktikum2021@users.noreply.github.com>
2023-12-06 08:13:50 +0100

    context test

--- src/GraphTool/GraphToolVisjsEvents.js
+++ src/GraphTool/GraphToolVisjsEvents.js
@@ -12,6 +12,7 @@ function initVisjsCallbacks () {
   })
 
   this.network.on('oncontext', (params) => {
+    params.event.preventDefault();
     this.visOnContext(params) // TODO: implement right click, probably Property-List, but as callback function.
   })
 
@@ -86,9 +87,126 @@ function visOnDragStart (params) {
     }
   }
 }
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function visOnContext (params) {
+  document.getElementById('Graph0_vis_container').addEventListener('mouseup', (e) => {
+    this.nodes.get().forEach((node) => {
+      if(node.contextCreated){
+        this.network.selectNodes([node.id], false);
+        this.nodes.remove(node.id)
+      }
+    })
+  })
+
+  var pointer = params.pointer.DOM;
+  let newNodes = [];
+  // Use getNodesAt to get the node IDs at the clicked position
+  var nodeIdArray = this.network.getNodeAt(pointer);
+  console.log(params)
+
+  let nodes = this.nodes.get()
+  let edges = this.edges.get()
+
+  // nodes.forEach((node) => {
+  //   node.color = 'white'
+  //   this.nodes.update(node)
+  // })
+
+  // edges.forEach((edge) => {
+  //   edge.color = 'black'
+  //   this.edges.update(edge)
+  // })
+  let manipulationNodes = ["AddNode", "Properties","AddEdge", "Delete"]
+
   console.log('in this.network.on(oncontext)')
+  console.log(this.network.body.nodes[nodeIdArray])
+// Function to add nodes around a specific node
+  let addNodesAroundCenter = async () => {
+    var numNewNodes = 4; // Number of new nodes to add
+    var radius = this.network.body.nodes[nodeIdArray].shape.radius * 2; // Radius of the circle around the center node
+
+    for (var i = 0; i < numNewNodes; i++) {
+
+      var angle = (i / numNewNodes) * 2 * Math.PI; //angle = (i / (numNewNodes - 1)) * (Math.PI / 2);
+      var x = radius * Math.cos(angle);
+      var y = radius * Math.sin(angle);
+
+      var newNode = {
+        id: manipulationNodes[0], // Assuming IDs start from 2
+        label: manipulationNodes[0],
+        x: params.pointer.canvas.x + x,
+        y: params.pointer.canvas.y + y,
+        fixed: true,
+        physics: false,
+        contextCreated: true,
+      };
+      newNodes.push(newNode.id);
+      this.nodes.update(newNode);
+      manipulationNodes.shift();
+
+      await delay(25)
+    }
+  }
+
+//   // Call the function to add nodes around the center
+   addNodesAroundCenter();
+   this.network.selectNodes([nodeIdArray], false);
+   this.network.selectNodes(newNodes, true);
+
+  let properties = ["AddNode1", "Properties1","AddEdge1", "Delete1"]
+  let hoverTimeout
+  let nodesGenerated = true
+
+  this.network.on('hoverNode', (params) => {
+    
+    const nodeId = params.node;
+    
+    let length = 4
+
+    hoverTimeout = setTimeout( () => {
+    nodesGenerated = !nodesGenerated
+
+    if(nodeId === "Properties" && !nodesGenerated){
+      for (var i = 0; i < length; i++) {
+        let newNode = {
+          id: properties[i], // Assuming IDs start from 2
+          label: properties[i],
+          x: this.network.getPosition(nodeId).x,
+          y: this.network.getPosition(nodeId).y + 50 * (i + 1),
+          fixed: true,
+          physics: false,
+          contextCreated: true,
+        };
+        this.nodes.update(newNode);
+        delay(25)
+      }
+
+    }
+
+    if(nodesGenerated && nodeId === "Properties"){
+      let reversedArray = [...properties].reverse();
+
+      reversedArray.forEach((node) => {
+        this.nodes.remove(node)
+        delay(25)
+      })
+
+    }
+  }, 500)
+
+  })
+
+  this.network.on('blurNode', (params) => {
+    
+    const nodeId = params.node
+
+    if(nodeId === "Properties"){
+      clearTimeout(hoverTimeout)
+    }
+  });
 }
 
 function visOnClick (params) {
